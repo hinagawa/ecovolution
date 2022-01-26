6321f13720803637ced1e51130f07d7837e6e9ba
hinagawa <psalimullina@gmail.com>
2022-01-26 22:16:17 +0300

    Reset password endpoint swagger

--- backend/swagger/routes/authRoutesInfo.js
+++ backend/swagger/routes/authRoutesInfo.js
@@ -74,6 +74,82 @@
  *         application/json:
  *          schema:
  *           $ref: '#/components/schemas/ResponseString'
+ * /api/forgot-password:
+ *   post:
+ *    tags:
+ *     - "Authorization"
+ *    summary: "Request to reset password by email"
+ *    consumes:
+ *     - application/x-www-form-urlencoded
+ *    produces:
+ *     - application/json
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: "User email"
+ *      required: true
+ *      schema:
+ *       $ref: '#/components/schemas/ForgotPassword'
+ *    responses:
+ *      200:
+ *        description: A succcessful response
+ *        content: 
+ *         application/json:
+ *          schema:
+ *           $ref: '#/components/schemas/ResponseString'
+ *      404:
+ *       description: User with this email does not exist
+ *       content: 
+ *        application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/ResponseString'
+ *      400:
+ *       description: Something went wrong
+ *       content: 
+ *        application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/ResponseString'
+ *      500:
+ *        description: Internal Server Error
+ *        content: 
+ *         application/json:
+ *          schema:
+ *           $ref: '#/components/schemas/ResponseString'
+ * /api/reset-password:
+ *  post:
+ *   tags:
+ *    - "Authorization"
+ *   summary: "Change password"
+ *   consumes:
+ *    - application/x-www-form-urlencoded
+ *   produces:
+ *    - application/json
+ *   parameters:
+ *   - in: "body"
+ *     name: "body"
+ *     description: "New user's password"
+ *     required: true
+ *     schema:
+ *      $ref: '#/components/schemas/ResetPassword'
+ *   responses:
+ *     200:
+ *       description: A succcessful response
+ *       content: 
+ *        application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/ResponseString'
+ *     400:
+ *      description: Invalid reset token
+ *      content: 
+ *       application/json:
+ *        schema:
+ *         $ref: '#/components/schemas/ResponseString'
+ *     500:
+ *       description: Internal Server Error
+ *       content: 
+ *        application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/ResponseString'
  */
 
 /**
@@ -97,6 +173,8 @@
  *           type: string 
  *     ResponseString:
  *      properties:
+ *        success:
+ *         type: boolean
  *        message: 
  *         type: string
  *     SignInInput:
@@ -108,5 +186,17 @@
  *           type: string
  *         password: 
  *           type: string   
+ *     ForgotPassword:
+ *      required:
+ *      - email
+ *      properties:
+ *       email:
+ *        type: string
+ *      ResetPassword:
+ *       required:
+ *       - newPassword
+ *       properties:
+ *        newPassword:
+ *         type: string
  *  
  */
