4d4e7c7f19911c4fdc7320d885ec4c9d1b219820
hinagawa <psalimullina@gmail.com>
2022-02-01 12:06:52 +0300

    Change production script

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -29,7 +30,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
-  }));
+}));
 
 app.use(
     cookieSession({
@@ -37,6 +38,13 @@ app.use(
         keys: keys.cookieKey
     })
 );
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
 
 app.use(passport.initialize());
 app.use(passport.session());
