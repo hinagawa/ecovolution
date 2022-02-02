6caa8f830b6d540b0acc84ad73a6a16cdf2904f3
hinagawa <psalimullina@gmail.com>
2022-02-02 19:58:27 +0300

    WIP api connect

--- frontend/src/pages/Auth/SignUpPage.jsx
+++ frontend/src/pages/Auth/SignUpPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 
 import img from '../../assets/images/img.png'
 
@@ -9,6 +10,31 @@ import Button from '../../components/Button/Button'
 import styles from './styles.module.css'
 
 function SignUpPage() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm()
+  // TODO fetch
+  const onSubmit = (data) => {
+    if(data.password !== data.confirmPassword) {
+      return 
+    }
+
+    console.log(data.lastName)
+  }
+  useEffect(() => {
+    register('name', { required: true })
+    register('lastName')
+    register('email')
+    register('password', { required: true, minLength: 8 })
+    register('confirmPassword', {
+      required: true,
+      minLength: 8,
+    })
+  }, [])
+
   return (
     <div className={styles.rowContainer}>
       <div className={styles.logoContainer}>
@@ -20,17 +46,53 @@ function SignUpPage() {
           <legend className={styles.legendFont}>
             Sign Up
           </legend>
-          <Form>
-            <Input placeholder='Name' />
-            <Input placeholder='E-mail' />
-            <Input placeholder='Password' />
-            <Input placeholder='Confirm password' />
-            <Button text='Sign Up' />
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              type='text'
+              placeholder='Name'
+              name='name'
+              onChange={(e) => {
+                setValue('name', e.target.value)
+              }}
+            />
+            <Input
+              type='text'
+              placeholder='Last Name'
+              name='lastName'
+              onChange={(e) => {
+                setValue('lastName', e.target.value)
+              }}
+            />
+            <Input
+              type='email'
+              placeholder='E-mail'
+              name='email'
+              onChange={(e) => {
+                setValue('email', e.target.value)
+              }}
+            />
+            <Input
+              type='password'
+              placeholder='Password'
+              name='password'
+              onChange={(e) => {
+                setValue('password', e.target.value)
+              }}
+            />
+            {errors.password && errors.password.message}
+            <Input
+              type='password'
+              placeholder='Confirm password'
+              name='confirmPassword'
+              onChange={(e) => {
+                setValue('confirmPassword', e.target.value)
+              }}
+            />
+            <Button>Submit</Button>
           </Form>
         </fieldset>
       </div>
     </div>
   )
 }
-
 export default SignUpPage
