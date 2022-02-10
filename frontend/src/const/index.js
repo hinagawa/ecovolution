cae4d8594ee02556812f8166037c060287dbfd08
hinagawa <psalimullina@gmail.com>
2022-02-10 19:54:53 +0300

    Fix API url in prod

--- frontend/src/const/index.js
+++ frontend/src/const/index.js
@@ -1,2 +1,2 @@
-const REACT_APP_BASE_URL = process.env.HOST || 'http://localhost:5000/'
+const REACT_APP_BASE_URL = process.env.NODE_ENV === 'production' ? process.env.HOST : 'http://localhost:5000/'
 export default REACT_APP_BASE_URL
