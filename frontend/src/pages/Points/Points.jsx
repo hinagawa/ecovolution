f76be58b030bc62f6d0a677b379eed455a4cb962
hinagawa <psalimullina@gmail.com>
2022-06-15 22:06:29 +0300

    Points

--- frontend/src/pages/Points/Points.jsx
+++ frontend/src/pages/Points/Points.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 
 import Header from '../../layouts/Header/Header'
+import Button from '../../components/Button/Button'
 
 import styles from './styles.module.css'
 
@@ -9,6 +10,24 @@ function Points() {
   return (
     <div className={styles.mainContainer}>
       <Header />
+      <div className={styles.pointContainer}>
+        <h1>Vegan Day</h1>
+        <p className={styles.placeDescription}>
+          Скидка 10% при заказе от 300 рублей
+        </p>
+        <Button>
+          1000 эко-баллов
+        </Button>
+      </div>
+      <div className={styles.pointContainer}>
+        <h1>Surf Coffee</h1>
+        <p className={styles.placeDescription}>
+          Бесплатный кофе
+        </p>
+        <Button>
+          2100 эко-баллов
+        </Button>
+      </div>
     </div>
   )
 }
